d1a210aa59e1ae09cb7680a5cf63e9e6f4ce3467
yauheni-beiduk <yauheni.beiduk@gmail.com>
2020-11-04 18:14:56 +0300

    fix:add script

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const arrowLeft = document.querySelector('.slider_left img');
+const arrowRight = document.querySelector('.slider_right img');
+
+arrowLeft.addEventListener('click', () => {
+    alert('Slide');
+  });
+
+arrowRight.addEventListener('click', ()=> {
+  alert('Slide');
+});
